aee9c12b409c99de52be38f62a6a093266c61655
Elias011 <elias.shkshb1991@gmail.com>
2018-08-10 01:09:21 +0200

    Elias is awesome

---
+++ eliasTask.js
@@ -0,0 +1,26 @@
+require('isomorphic-fetch');
+
+const fetchScore = async (i) => {
+  try {
+    const url2 = await `https://hacker-news.firebaseio.com/v0/item/${i}.json?print=pretty`;
+    const response2 = await fetch(url2);
+    console.log(await response2.text())
+  } catch (error) {
+    console.log('fetch failed', err);
+  }
+}
+
+const logFetch = async (url) => {
+  try {
+    const response = await fetch(url);
+    const topStories = await response.text()
+    const arrayTopStories = topStories.split(',').map(Number);
+   await arrayTopStories.map (i => {
+      fetchScore (i)
+    })
+  }
+  catch (err) {
+    console.log('fetch failed', err);
+  }
+}
+logFetch(`https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`)
